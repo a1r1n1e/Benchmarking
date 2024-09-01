43e14430ad031142634bb536d7adb7d2909d72ad
Vladimir Arnold <vovch2ara@gmail.com>
2024-09-01 19:41:23 +0300

    Object properties amount enriched & Alternative Concurrency handler added

--- Benchmarkable.cs
+++ Benchmarkable.cs
@@ -1,5 +1,7 @@
-using System;
+using System.Reflection;
+using System.Collections.Concurrent;
 using BenchmarkDotNet.Attributes;
+using Newtonsoft.Json;
 
 namespace Benchmarking;
 
@@ -9,53 +11,95 @@ public class Benchmarkable
 
     static Benchmarkable()
     {
-        Obj = new POCO()
-        {
-            StringProperty1 = "sdfsdasfddfs",
-            StringProperty2 = "sdfsdgffs",
-            StringProperty3 = "syfgddfs",
-            StringProperty4 = "sdfasdsas",
-            StringProperty5 = "sdfsadad",
-            IntProperty1 = 125,
-            IntProperty2 = 178525,
-            IntProperty3 = 12565,
-            IntProperty4 = 568125,
-            IntProperty5 = 568564125
-        };
+        Obj = POCO.GetRandomInstance();
+        _lockObject = new object();
+        _knownTypes = new Dictionary<Type, PropertyInfo[]>();
+        _knownTypesConcurrent = new ConcurrentDictionary<Type, PropertyInfo[]>();
     }
 
     [Benchmark]
     public Dictionary<string, object> WithReflection() => WithReflection(Obj);
     public Dictionary<string, object> WithReflection(POCO obj)
     {
-        return new Dictionary<string, object>();
+        var result = new Dictionary<string, object>();
+
+        var properties = obj.GetType().GetProperties();
+        foreach(var prop in properties)
+        {
+            result.Add(prop.Name, prop.GetValue(obj, null));
+        }
+
+        return result;
     }
 
+    protected static Dictionary<Type, PropertyInfo[]> _knownTypes;
+
+    protected static object _lockObject;
+
     [Benchmark]
-    public Dictionary<string, object> WithReflectionAndStatics() => WithReflectionAndStatics(Obj);
-    public Dictionary<string, object> WithReflectionAndStatics(POCO obj)
+    public Dictionary<string, object> WithReflectionAndStaticLockedDictionary() => WithReflectionAndStaticLockedDictionary(Obj);
+    public Dictionary<string, object> WithReflectionAndStaticLockedDictionary(POCO obj)
     {
-        return new Dictionary<string, object>();
+        var result = new Dictionary<string, object>();
+
+        var type = obj.GetType();
+
+        PropertyInfo[] properties = Array.Empty<PropertyInfo>();
+        lock(_lockObject)
+        {
+            if(!_knownTypes.TryGetValue(type, out properties))
+            {
+                properties = type.GetProperties();
+                _knownTypes.Add(type, properties);
+
+            }
+        }
+
+        foreach(var prop in properties)
+        {
+            result.Add(prop.Name, prop.GetValue(obj, null));
+        }
+
+        return result;
+    }
+
+    protected static ConcurrentDictionary<Type, PropertyInfo[]> _knownTypesConcurrent;
+
+    [Benchmark]
+    public Dictionary<string, object> WithReflectionAndStaticConcurrentDictionary() => WithReflectionAndStaticConcurrentDictionary(Obj);
+    public Dictionary<string, object> WithReflectionAndStaticConcurrentDictionary(POCO obj)
+    {
+        var result = new Dictionary<string, object>();
+
+        var type = obj.GetType();
+
+        var properties = _knownTypesConcurrent.GetOrAdd(type, x => type.GetProperties());
+
+        foreach(var prop in properties)
+        {
+            result.Add(prop.Name, prop.GetValue(obj, null));
+        }
+
+        return result;
     }
 
     [Benchmark]
     public Dictionary<string, object> WithNewtonsoft() => WithNewtonsoft(Obj);
     public Dictionary<string, object> WithNewtonsoft(POCO obj)
     {
-        return new Dictionary<string, object>();
+        var result = JsonConvert.DeserializeObject<Dictionary<string, object>>
+        (JsonConvert.SerializeObject(obj, Formatting.Indented));
+
+        return result;
     }
 
     [Benchmark]
     public Dictionary<string, object> WithSystemJson() => WithSystemJson(Obj);
     public Dictionary<string, object> WithSystemJson(POCO obj)
     {
-        return new Dictionary<string, object>();
-    }
+        var result = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, object>>
+        (System.Text.Json.JsonSerializer.Serialize(obj));
 
-    [Benchmark]
-    public Dictionary<string, object> WithAttributesMarkings() => WithAttributesMarkings(Obj);
-    public Dictionary<string, object> WithAttributesMarkings(POCO obj)
-    {
-        return new Dictionary<string, object>();
+        return result;
     }
 }
--- POCO.cs
+++ POCO.cs
@@ -1,23 +1,414 @@
-using System;
-
 namespace Benchmarking;
 
 public class POCO
 {
     public string StringProperty1 {get; set;}
-
     public string StringProperty2 {get; set;}
     public string StringProperty3 {get; set;}
     public string StringProperty4 {get; set;}
     public string StringProperty5 {get; set;}
+    public string StringProperty6 {get; set;}
+    public string StringProperty7 {get; set;}
+    public string StringProperty8 {get; set;}
+    public string StringProperty9 {get; set;}
+    public string StringProperty10 {get; set;}
+    public string StringProperty11 {get; set;}
+    public string StringProperty12 {get; set;}
+    public string StringProperty13 {get; set;}
+    public string StringProperty14 {get; set;}
+    public string StringProperty15 {get; set;}
+    public string StringProperty16 {get; set;}
+    public string StringProperty17 {get; set;}
+    public string StringProperty18 {get; set;}
+    public string StringProperty19 {get; set;}
+    public string StringProperty20 {get; set;}
+    public string StringProperty21 {get; set;}
+    public string StringProperty22 {get; set;}
+    public string StringProperty23 {get; set;}
+    public string StringProperty24 {get; set;}
+    public string StringProperty25 {get; set;}
+    public string StringProperty26 {get; set;}
+    public string StringProperty27 {get; set;}
+    public string StringProperty28 {get; set;}
+    public string StringProperty29 {get; set;}
+    public string StringProperty30 {get; set;}
+    public string StringProperty31 {get; set;}
+    public string StringProperty32 {get; set;}
+    public string StringProperty33 {get; set;}
+    public string StringProperty34 {get; set;}
+    public string StringProperty35 {get; set;}
+    public string StringProperty36 {get; set;}
+    public string StringProperty37 {get; set;}
+    public string StringProperty38 {get; set;}
+    public string StringProperty39 {get; set;}
+    public string StringProperty40 {get; set;}
+    public string StringProperty41 {get; set;}
+    public string StringProperty42 {get; set;}
+    public string StringProperty43 {get; set;}
+    public string StringProperty44 {get; set;}
+    public string StringProperty45 {get; set;}
+    public string StringProperty46 {get; set;}
+    public string StringProperty47 {get; set;}
+    public string StringProperty48 {get; set;}
+    public string StringProperty49 {get; set;}
+    public string StringProperty50 {get; set;}
+    public string StringProperty51 {get; set;}
+    public string StringProperty52 {get; set;}
+    public string StringProperty53 {get; set;}
+    public string StringProperty54 {get; set;}
+    public string StringProperty55 {get; set;}
+    public string StringProperty56 {get; set;}
+    public string StringProperty57 {get; set;}
+    public string StringProperty58 {get; set;}
+    public string StringProperty59 {get; set;}
+    public string StringProperty60 {get; set;}
+    public string StringProperty61 {get; set;}
+    public string StringProperty62 {get; set;}
+    public string StringProperty63 {get; set;}
+    public string StringProperty64 {get; set;}
+    public string StringProperty65 {get; set;}
+    public string StringProperty66 {get; set;}
+    public string StringProperty67 {get; set;}
+    public string StringProperty68 {get; set;}
+    public string StringProperty69 {get; set;}
+    public string StringProperty70 {get; set;}
+    public string StringProperty71 {get; set;}
+    public string StringProperty72 {get; set;}
+    public string StringProperty73 {get; set;}
+    public string StringProperty74 {get; set;}
+    public string StringProperty75 {get; set;}
+    public string StringProperty76 {get; set;}
+    public string StringProperty77 {get; set;}
+    public string StringProperty78 {get; set;}
+    public string StringProperty79 {get; set;}
+    public string StringProperty80 {get; set;}
+    public string StringProperty81 {get; set;}
+    public string StringProperty82 {get; set;}
+    public string StringProperty83 {get; set;}
+    public string StringProperty84 {get; set;}
+    public string StringProperty85 {get; set;}
+    public string StringProperty86 {get; set;}
+    public string StringProperty87 {get; set;}
+    public string StringProperty88 {get; set;}
+    public string StringProperty89 {get; set;}
+    public string StringProperty90 {get; set;}
+    public string StringProperty91 {get; set;}
+    public string StringProperty92 {get; set;}
+    public string StringProperty93 {get; set;}
+    public string StringProperty94 {get; set;}
+    public string StringProperty95 {get; set;}
+    public string StringProperty96 {get; set;}
+    public string StringProperty97 {get; set;}
+    public string StringProperty98 {get; set;}
+    public string StringProperty99 {get; set;}
+    public string StringProperty100 {get; set;}
 
     public int IntProperty1 {get; set;}
-
     public int IntProperty2 {get; set;}
-
     public int IntProperty3 {get; set;}
-
     public int IntProperty4 {get; set;}
-
     public int IntProperty5 {get; set;}
+    public int IntProperty6 {get; set;}
+    public int IntProperty7 {get; set;}
+    public int IntProperty8 {get; set;}
+    public int IntProperty9 {get; set;}
+    public int IntProperty10 {get; set;}
+    public int IntProperty11 {get; set;}
+    public int IntProperty12 {get; set;}
+    public int IntProperty13 {get; set;}
+    public int IntProperty14 {get; set;}
+    public int IntProperty15 {get; set;}
+    public int IntProperty16 {get; set;}
+    public int IntProperty17 {get; set;}
+    public int IntProperty18 {get; set;}
+    public int IntProperty19 {get; set;}
+    public int IntProperty20 {get; set;}
+    public int IntProperty21 {get; set;}
+    public int IntProperty22 {get; set;}
+    public int IntProperty23 {get; set;}
+    public int IntProperty24 {get; set;}
+    public int IntProperty25 {get; set;}
+    public int IntProperty26 {get; set;}
+    public int IntProperty27 {get; set;}
+    public int IntProperty28 {get; set;}
+    public int IntProperty29 {get; set;}
+    public int IntProperty30 {get; set;}
+    public int IntProperty31 {get; set;}
+    public int IntProperty32 {get; set;}
+    public int IntProperty33 {get; set;}
+    public int IntProperty34 {get; set;}
+    public int IntProperty35 {get; set;}
+    public int IntProperty36 {get; set;}
+    public int IntProperty37 {get; set;}
+    public int IntProperty38 {get; set;}
+    public int IntProperty39 {get; set;}
+    public int IntProperty40 {get; set;}
+    public int IntProperty41 {get; set;}
+    public int IntProperty42 {get; set;}
+    public int IntProperty43 {get; set;}
+    public int IntProperty44 {get; set;}
+    public int IntProperty45 {get; set;}
+    public int IntProperty46 {get; set;}
+    public int IntProperty47 {get; set;}
+    public int IntProperty48 {get; set;}
+    public int IntProperty49 {get; set;}
+    public int IntProperty50 {get; set;}
+    public int IntProperty51 {get; set;}
+    public int IntProperty52 {get; set;}
+    public int IntProperty53 {get; set;}
+    public int IntProperty54 {get; set;}
+    public int IntProperty55 {get; set;}
+    public int IntProperty56 {get; set;}
+    public int IntProperty57 {get; set;}
+    public int IntProperty58 {get; set;}
+    public int IntProperty59 {get; set;}
+    public int IntProperty60 {get; set;}
+    public int IntProperty61 {get; set;}
+    public int IntProperty62 {get; set;}
+    public int IntProperty63 {get; set;}
+    public int IntProperty64 {get; set;}
+    public int IntProperty65 {get; set;}
+    public int IntProperty66 {get; set;}
+    public int IntProperty67 {get; set;}
+    public int IntProperty68 {get; set;}
+    public int IntProperty69 {get; set;}
+    public int IntProperty70 {get; set;}
+    public int IntProperty71 {get; set;}
+    public int IntProperty72 {get; set;}
+    public int IntProperty73 {get; set;}
+    public int IntProperty74 {get; set;}
+    public int IntProperty75 {get; set;}
+    public int IntProperty76 {get; set;}
+    public int IntProperty77 {get; set;}
+    public int IntProperty78 {get; set;}
+    public int IntProperty79 {get; set;}
+    public int IntProperty80 {get; set;}
+    public int IntProperty81 {get; set;}
+    public int IntProperty82 {get; set;}
+    public int IntProperty83 {get; set;}
+    public int IntProperty84 {get; set;}
+    public int IntProperty85 {get; set;}
+    public int IntProperty86 {get; set;}
+    public int IntProperty87 {get; set;}
+    public int IntProperty88 {get; set;}
+    public int IntProperty89 {get; set;}
+    public int IntProperty90 {get; set;}
+    public int IntProperty91 {get; set;}
+    public int IntProperty92 {get; set;}
+    public int IntProperty93 {get; set;}
+    public int IntProperty94 {get; set;}
+    public int IntProperty95 {get; set;}
+    public int IntProperty96 {get; set;}
+    public int IntProperty97 {get; set;}
+    public int IntProperty98 {get; set;}
+    public int IntProperty99 {get; set;}
+    public int IntProperty100 {get; set;}
+
+    public static POCO GetRandomInstance()
+    {
+        Random random = new Random();
+        return new POCO()
+        {
+            StringProperty1 = Guid.NewGuid().ToString(),
+            StringProperty2 = Guid.NewGuid().ToString(),
+            StringProperty3 = Guid.NewGuid().ToString(),
+            StringProperty4 = Guid.NewGuid().ToString(),
+            StringProperty5 = Guid.NewGuid().ToString(),
+            StringProperty6 = Guid.NewGuid().ToString(),
+            StringProperty7 = Guid.NewGuid().ToString(),
+            StringProperty8 = Guid.NewGuid().ToString(),
+            StringProperty9 = Guid.NewGuid().ToString(),
+            StringProperty10 = Guid.NewGuid().ToString(),
+            StringProperty11 = Guid.NewGuid().ToString(),
+            StringProperty12 = Guid.NewGuid().ToString(),
+            StringProperty13 = Guid.NewGuid().ToString(),
+            StringProperty14 = Guid.NewGuid().ToString(),
+            StringProperty15 = Guid.NewGuid().ToString(),
+            StringProperty16 = Guid.NewGuid().ToString(),
+            StringProperty17 = Guid.NewGuid().ToString(),
+            StringProperty18 = Guid.NewGuid().ToString(),
+            StringProperty19 = Guid.NewGuid().ToString(),
+            StringProperty20 = Guid.NewGuid().ToString(),
+            StringProperty21 = Guid.NewGuid().ToString(),
+            StringProperty22 = Guid.NewGuid().ToString(),
+            StringProperty23 = Guid.NewGuid().ToString(),
+            StringProperty24 = Guid.NewGuid().ToString(),
+            StringProperty25 = Guid.NewGuid().ToString(),
+            StringProperty26 = Guid.NewGuid().ToString(),
+            StringProperty27 = Guid.NewGuid().ToString(),
+            StringProperty28 = Guid.NewGuid().ToString(),
+            StringProperty29 = Guid.NewGuid().ToString(),
+            StringProperty30 = Guid.NewGuid().ToString(),
+            StringProperty31 = Guid.NewGuid().ToString(),
+            StringProperty32 = Guid.NewGuid().ToString(),
+            StringProperty33 = Guid.NewGuid().ToString(),
+            StringProperty34 = Guid.NewGuid().ToString(),
+            StringProperty35 = Guid.NewGuid().ToString(),
+            StringProperty36 = Guid.NewGuid().ToString(),
+            StringProperty37 = Guid.NewGuid().ToString(),
+            StringProperty38 = Guid.NewGuid().ToString(),
+            StringProperty39 = Guid.NewGuid().ToString(),
+            StringProperty40 = Guid.NewGuid().ToString(),
+            StringProperty41 = Guid.NewGuid().ToString(),
+            StringProperty42 = Guid.NewGuid().ToString(),
+            StringProperty43 = Guid.NewGuid().ToString(),
+            StringProperty44 = Guid.NewGuid().ToString(),
+            StringProperty45 = Guid.NewGuid().ToString(),
+            StringProperty46 = Guid.NewGuid().ToString(),
+            StringProperty47 = Guid.NewGuid().ToString(),
+            StringProperty48 = Guid.NewGuid().ToString(),
+            StringProperty49 = Guid.NewGuid().ToString(),
+            StringProperty50 = Guid.NewGuid().ToString(),
+            StringProperty51 = Guid.NewGuid().ToString(),
+            StringProperty52 = Guid.NewGuid().ToString(),
+            StringProperty53 = Guid.NewGuid().ToString(),
+            StringProperty54 = Guid.NewGuid().ToString(),
+            StringProperty55 = Guid.NewGuid().ToString(),
+            StringProperty56 = Guid.NewGuid().ToString(),
+            StringProperty57 = Guid.NewGuid().ToString(),
+            StringProperty58 = Guid.NewGuid().ToString(),
+            StringProperty59 = Guid.NewGuid().ToString(),
+            StringProperty60 = Guid.NewGuid().ToString(),
+            StringProperty61 = Guid.NewGuid().ToString(),
+            StringProperty62 = Guid.NewGuid().ToString(),
+            StringProperty63 = Guid.NewGuid().ToString(),
+            StringProperty64 = Guid.NewGuid().ToString(),
+            StringProperty65 = Guid.NewGuid().ToString(),
+            StringProperty66 = Guid.NewGuid().ToString(),
+            StringProperty67 = Guid.NewGuid().ToString(),
+            StringProperty68 = Guid.NewGuid().ToString(),
+            StringProperty69 = Guid.NewGuid().ToString(),
+            StringProperty70 = Guid.NewGuid().ToString(),
+            StringProperty71 = Guid.NewGuid().ToString(),
+            StringProperty72 = Guid.NewGuid().ToString(),
+            StringProperty73 = Guid.NewGuid().ToString(),
+            StringProperty74 = Guid.NewGuid().ToString(),
+            StringProperty75 = Guid.NewGuid().ToString(),
+            StringProperty76 = Guid.NewGuid().ToString(),
+            StringProperty77 = Guid.NewGuid().ToString(),
+            StringProperty78 = Guid.NewGuid().ToString(),
+            StringProperty79 = Guid.NewGuid().ToString(),
+            StringProperty80 = Guid.NewGuid().ToString(),
+            StringProperty81 = Guid.NewGuid().ToString(),
+            StringProperty82 = Guid.NewGuid().ToString(),
+            StringProperty83 = Guid.NewGuid().ToString(),
+            StringProperty84 = Guid.NewGuid().ToString(),
+            StringProperty85 = Guid.NewGuid().ToString(),
+            StringProperty86 = Guid.NewGuid().ToString(),
+            StringProperty87 = Guid.NewGuid().ToString(),
+            StringProperty88 = Guid.NewGuid().ToString(),
+            StringProperty89 = Guid.NewGuid().ToString(),
+            StringProperty90 = Guid.NewGuid().ToString(),
+            StringProperty91 = Guid.NewGuid().ToString(),
+            StringProperty92 = Guid.NewGuid().ToString(),
+            StringProperty93 = Guid.NewGuid().ToString(),
+            StringProperty94 = Guid.NewGuid().ToString(),
+            StringProperty95 = Guid.NewGuid().ToString(),
+            StringProperty96 = Guid.NewGuid().ToString(),
+            StringProperty97 = Guid.NewGuid().ToString(),
+            StringProperty98 = Guid.NewGuid().ToString(),
+            StringProperty99 = Guid.NewGuid().ToString(),
+            StringProperty100 = Guid.NewGuid().ToString(),
+            IntProperty1 = random.Next(),
+            IntProperty2 = random.Next(),
+            IntProperty3 = random.Next(),
+            IntProperty4 = random.Next(),
+            IntProperty5 = random.Next(),
+            IntProperty6 = random.Next(),
+            IntProperty7 = random.Next(),
+            IntProperty8 = random.Next(),
+            IntProperty9 = random.Next(),
+            IntProperty10 = random.Next(),
+            IntProperty11 = random.Next(),
+            IntProperty12 = random.Next(),
+            IntProperty13 = random.Next(),
+            IntProperty14 = random.Next(),
+            IntProperty15 = random.Next(),
+            IntProperty16 = random.Next(),
+            IntProperty17 = random.Next(),
+            IntProperty18 = random.Next(),
+            IntProperty19 = random.Next(),
+            IntProperty20 = random.Next(),
+            IntProperty21 = random.Next(),
+            IntProperty22 = random.Next(),
+            IntProperty23 = random.Next(),
+            IntProperty24 = random.Next(),
+            IntProperty25 = random.Next(),
+            IntProperty26 = random.Next(),
+            IntProperty27 = random.Next(),
+            IntProperty28 = random.Next(),
+            IntProperty29 = random.Next(),
+            IntProperty30 = random.Next(),
+            IntProperty31 = random.Next(),
+            IntProperty32 = random.Next(),
+            IntProperty33 = random.Next(),
+            IntProperty34 = random.Next(),
+            IntProperty35 = random.Next(),
+            IntProperty36 = random.Next(),
+            IntProperty37 = random.Next(),
+            IntProperty38 = random.Next(),
+            IntProperty39 = random.Next(),
+            IntProperty40 = random.Next(),
+            IntProperty41 = random.Next(),
+            IntProperty42 = random.Next(),
+            IntProperty43 = random.Next(),
+            IntProperty44 = random.Next(),
+            IntProperty45 = random.Next(),
+            IntProperty46 = random.Next(),
+            IntProperty47 = random.Next(),
+            IntProperty48 = random.Next(),
+            IntProperty49 = random.Next(),
+            IntProperty50 = random.Next(),
+            IntProperty51 = random.Next(),
+            IntProperty52 = random.Next(),
+            IntProperty53 = random.Next(),
+            IntProperty54 = random.Next(),
+            IntProperty55 = random.Next(),
+            IntProperty56 = random.Next(),
+            IntProperty57 = random.Next(),
+            IntProperty58 = random.Next(),
+            IntProperty59 = random.Next(),
+            IntProperty60 = random.Next(),
+            IntProperty61 = random.Next(),
+            IntProperty62 = random.Next(),
+            IntProperty63 = random.Next(),
+            IntProperty64 = random.Next(),
+            IntProperty65 = random.Next(),
+            IntProperty66 = random.Next(),
+            IntProperty67 = random.Next(),
+            IntProperty68 = random.Next(),
+            IntProperty69 = random.Next(),
+            IntProperty70 = random.Next(),
+            IntProperty71 = random.Next(),
+            IntProperty72 = random.Next(),
+            IntProperty73 = random.Next(),
+            IntProperty74 = random.Next(),
+            IntProperty75 = random.Next(),
+            IntProperty76 = random.Next(),
+            IntProperty77 = random.Next(),
+            IntProperty78 = random.Next(),
+            IntProperty79 = random.Next(),
+            IntProperty80 = random.Next(),
+            IntProperty81 = random.Next(),
+            IntProperty82 = random.Next(),
+            IntProperty83 = random.Next(),
+            IntProperty84 = random.Next(),
+            IntProperty85 = random.Next(),
+            IntProperty86 = random.Next(),
+            IntProperty87 = random.Next(),
+            IntProperty88 = random.Next(),
+            IntProperty89 = random.Next(),
+            IntProperty90 = random.Next(),
+            IntProperty91 = random.Next(),
+            IntProperty92 = random.Next(),
+            IntProperty93 = random.Next(),
+            IntProperty94 = random.Next(),
+            IntProperty95 = random.Next(),
+            IntProperty96 = random.Next(),
+            IntProperty97 = random.Next(),
+            IntProperty98 = random.Next(),
+            IntProperty99 = random.Next(),
+            IntProperty100 = random.Next()
+        };
+    }
 }
